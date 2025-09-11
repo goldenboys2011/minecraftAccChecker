2f0bde9aff2c94a91e24181509d8cf6cc3264272
goldenboys2011 <ody.chryssos@gmail.com>
2025-09-11 22:01:19 +0300

    fixed flask start

--- server.py
+++ server.py
@@ -145,4 +145,4 @@ def analyze():
     return jsonify(calculate(username))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
